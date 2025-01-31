9ee79c0f351768b98b376d825c8424d6ac4f596c
Sandesh-Pol <sandeshpol268@gmail.com>
2025-01-31 22:26:58 +0530

    api setup

--- yt_comment_analyzer/analysis/src/config/settings.py
+++ yt_comment_analyzer/analysis/src/config/settings.py
@@ -1,3 +1,3 @@
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
-YOUTUBE_API_KEY = "AIzaSyBmkRo--2j9zLcyujQdhiGXnTvu_3M_TlI"  # Replace with your API key
+YOUTUBE_API_KEY = "AIzaSyBmkRo--2j9zLcyujQdhiGXnTvu_3M_TlI"  
